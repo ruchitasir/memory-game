389e0736dd48c9ac871f29aa6488e3eed7714b62
skhaitan <skhaitan@skhaitans-MacBook-Pro.local>
2020-03-04 15:46:40 -0800

    created game logic with control flow

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,3 +1,6 @@
+/*
+// Intro to JS assignment
+
 let cardOne = "queen";
 let cardTwo = "queen";
 let cardThree = "king";
@@ -5,3 +8,33 @@ let cardFour = "king";
 
 console.log("User flipped " +cardOne);
 console.log("User flipped " +cardThree);
+*/
+
+
+// Control flow assignment
+
+cards= ["queen","queen","king","king"]; 
+
+cardsInPlay = [];
+
+cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped " +cardOne);
+
+cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+console.log("User flipped " +cardTwo);
+
+if(cardsInPlay.length ===2)
+{
+	if(cardsInPlay[0] === cardsInPlay[1])
+	{
+		alert("You found a match!");
+	}
+	else
+	{
+		alert("Sorry, try again.");
+	}
+}
