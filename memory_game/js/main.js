53d2c637cba8a7c6100e4b3253bd834de82d6b10
skhaitan <skhaitan@skhaitans-MacBook-Pro.local>
2020-03-05 14:28:32 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -68,6 +68,7 @@ function flipCard(cardId)
 
  // Objects assignment
 
+/*
 const cards = [{rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"}, 
 {rank: "queen", suit:"diamonds", cardImage: "images/queen-of-diamonds.png"}, 
 {rank: "king", suit:"hearts", cardImage: "images/king-of-hearts.png"}, 
@@ -101,13 +102,64 @@ function flipCard(cardId)
 	}
 
 }
-
  flipCard(0);
  flipCard(2);
+*/
 
+// DOM assignment
+const cards = [{rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"}, 
+{rank: "queen", suit:"diamonds", cardImage: "images/queen-of-diamonds.png"}, 
+{rank: "king", suit:"hearts", cardImage: "images/king-of-hearts.png"}, 
+{rank: "king", suit:"diamonds", cardImage: "images/king-of-diamonds.png"}]; 
 
+let cardsInPlay = [];
 
+function  checkForMatch(){
+ 	
+  	 if(cardsInPlay[0].rank === cardsInPlay[1].rank){
+  	 	if(cardsInPlay[0].suit !== cardsInPlay[1].suit){
+  			alert("You found a match!");
+  	 	} 		
+  	 	else{
+  	 		alert("You have exhausted your two clicks by clicking the same card. Press reset to start over");
+  	 	}
+	} 
+	else {
+  		
+  		alert("Sorry, try again.");
+	}
+ }
 
+var gameBoardDiv= document.getElementById('game-board');
+function createBoard(){
+	for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement('img');
+     cardElement.setAttribute('src','images/back.png');
+     cardElement.setAttribute('data-id',i);
+     cardElement.addEventListener('click',flipCard);
+     gameBoardDiv.appendChild(cardElement);
+	}
+}
+
+function flipCard()
+{
+	cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("Card Image " + cards[cardId].cardImage);
+
+	console.log("Suit " + cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	cardsInPlay.push(cards[cardId]);
+
+	if(cardsInPlay.length ===2)
+	{
+		checkForMatch();
+	}
+
+}
+
+createBoard();
 
 
 
